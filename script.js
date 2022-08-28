a1d6d202c1dcf1e762ca13777887f61e0812cd4b
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2022-08-27 21:51:37 -0300

    criando os movimentos da cobrinha

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ snake[0] = {
     y: 8 * box,
 }
 let direction = "rigth";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
-
+/* ----criando a cobrinha------*/
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
@@ -20,16 +24,53 @@ function criarCobrinha(){
     }
 
 }
+/* ----criando comida------*/
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 
+}
+
+document.addEventListener("keydown", update);
+
+/* ------ criando os comandos do tecladro -----*/
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+/* ------- criando os movimento para a cobrinha aparecer do outro lado da caixa -----*/
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
+    
+/* ------- criando os movimentos da cbrinha -----*/
      let snakeX = snake[0].x; 
      let snakeY = snake[0].y;
 
-     
+     if(direction == "right") snakeX += box;
+     if(direction == "left") snakeX -= box;
+     if(direction == "up") snakeY -= box;
+     if(direction == "down") snakeY += box;
+
+     snake.pop();
+
+     let newHead = {
+        x: snakeX,
+        y: snakeY 
+     }
+
+     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 250);
  
